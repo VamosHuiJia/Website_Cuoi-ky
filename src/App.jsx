5a8aee2b1e7ca7e399ed3072905a174dfefd7e73
Huy <giahuy20092004@gmail.com>
2025-04-21 17:01:07 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
-import ContactUs from "./components/contact/ContactUs";
-import SignIn from "./components/signin/SignIn";
-import Products from "./components/product/Product";
-import ProductCard from "./components/product/ProductCard";
-import Home from "./components/home/HomePage"; // Import Home component
+// src/App.jsx
+import React from 'react';
+import Header from './components/header/Header'; 
+import Footer from './components/footer/Footer';
+import SignIn from './Log_in/Sign_in/Sign_in';
+import Home from "./pages/home/HomePage";
+import AboutUs from './pages/about_us/aboutus'; 
+import ContactUs from "./pages/contact/Contactus";
+import Products from "./pages/product/Product";
+import ProductCard from "./pages/product/ProductCard";
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 
 const App = () => {
   return (
@@ -22,7 +25,7 @@ const App = () => {
             <Route path="/products" element={<Products />} />
             <Route path="/product/:id" element={<ProductCard />} />
             <Route path="/categories" element={<h1>Categories Page</h1>} />
-            <Route path="/about" element={<h1>About Page</h1>} />
+            <Route path="/about" element={<AboutUs />} />
             <Route path="/contact" element={<ContactUs />} />
             <Route path="/signin" element={<SignIn />} />
           </Routes>
